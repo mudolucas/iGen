1b58b6d46d1b50bd4453dfda41595d137facc85f
Vivian Hua <v-vivhu@microsoft.com>
2019-07-24 11:08:06 -0700

    started pin code

--- iGen/iGen/ViewController.swift
+++ iGen/iGen/ViewController.swift
@@ -16,6 +16,8 @@ import FirebaseAnalytics
 class ViewController: UIViewController {
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var pinTextField: UITextField!
+    
   
     @IBAction func pressCreateAccount(_ sender: Any) {
     }
@@ -97,11 +99,49 @@ class ViewController: UIViewController {
         }
     }
     
-    
+    @IBAction func setPinPressed(_ sender: Any) {
+        
+        if (pinTextField.text == nil || pinTextField.text?.count != 4) {
+            //Alert to tell the user that there was an error because they didn't fill anything in the textfields because they didn't fill anything in
+            let alertController = UIAlertController(title: "Error", message: "Please enter a 4 digit pin.", preferredStyle: .alert)
+                
+            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alertController.addAction(defaultAction)
+                
+            self.present(alertController, animated: true, completion: nil)
+        } else {
+            
+            Auth.auth().signIn(withEmail: loginEmail.text!, password: loginPassword.text!) { (user, error) in
+                
+                if error == nil {
+                    
+                    //Print into the console if successfully logged in
+                    print("You have successfully logged in")
+                    
+                    //Go to the HomeViewController if the login is sucessful
+                    let vc = self.storyboard?.instantiateViewController(withIdentifier: "Home")
+                    self.present(vc!, animated: true, completion: nil)
+                    
+                } else {
+                    
+                    //Tells the user that there is an error and then gets firebase to tell them the error
+                    let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+                    
+                    let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                    alertController.addAction(defaultAction)
+                    
+                    self.present(alertController, animated: true, completion: nil)
+                }
+            }
+        }
+        
+    }
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        pinTextField.keyboardType = .numberPad
     }
 
 }
