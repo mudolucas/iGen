c959357f255fe1fd3badb6d686926459b00cdb9d
Lia Johansen <lxjohansen@gmail.com>
2019-07-26 10:36:21 -0700

    working on the switch button

--- iGen/iGen/MyCustomTableViewCell.swift
+++ iGen/iGen/MyCustomTableViewCell.swift
@@ -23,14 +23,18 @@ class MyCustomTableViewCell: UITableViewCell {
         // Configure the view for the selected state
     }
     
+    // SWITCH: PARENT TO KID AND KID TO PARENT
     @IBAction func kidModeSwitch(_ sender: UISwitch) {
         if sender.isOn {
             print("button is pressed to on AKA childMode is ON!!!")
         } else {
             print("button is pressed to OFF --> go into the popup")
+            // sender.addTarget(<#T##target: Any?##Any?#>, action: <#T##Selector#>, for: <#T##UIControl.Event#>)
+            
+            
+            
         }
     }
-    
-    
-    
 }
+
+
