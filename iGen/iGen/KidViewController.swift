07b871e634c98f71ebe4ca46faf830f6f99ea0ac
Sara Guizani <sarasponda23@gmail.com>
2019-07-26 10:39:46 -0700

    Tommi kid interface began

---
+++ iGen/iGen/KidViewController.swift
@@ -0,0 +1,13 @@
+//
+//  KidViewController.swift
+//  iGen
+//
+//  Created by Sara Guizani on 7/26/19.
+//  Copyright © 2019 Lucas Mudo de Araujo. All rights reserved.
+//
+
+import UIKit
+
+class KidViewController: UIViewController {
+    
+}
