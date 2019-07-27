01be0c482471de8a7adbb653c38266cb736f0631
Vivian Hua <v-vivhu@microsoft.com>
2019-07-27 11:20:27 -0700

    added set pin to flow

--- iGen/iGen/liaAlertPasswordViewController.swift
+++ iGen/iGen/liaAlertPasswordViewController.swift
@@ -9,44 +9,22 @@
 import UIKit
 
 class liaAlertPasswordViewController: UIViewController {
-
+    @IBOutlet weak var kidsModeButton: UISwitch!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        createAlert(title: "testing", message: "does this work")
         // Do any additional setup after loading the view.
     }
     
-    
-    
-    func createAlert(title:String, message:String) {
-        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
-        
-        
-        // CREATING THOSE CANCEL OR OKAY BUTTONS
-        alert.addAction(UIAlertAction(title: "CANCEL", style: UIAlertAction.Style.default, handler: { (action) in
-            alert.dismiss(animated: true, completion: nil)
-        }))
-        alert.addAction(UIAlertAction(title: "SUBMIT", style: UIAlertAction.Style.default, handler: { (action) in
-            alert.dismiss(animated: true, completion: nil)
-            // COMPARE THE PASSCODE
-        }))
-        
-        
-        self.present(alert, animated: true, completion: nil)
-        
+    @IBAction func switchPressed(_ sender: UISwitch) {
+        if kidsModeButton.isOn {
+            
+        } else {
+            
+        }
     }
     
-    
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
